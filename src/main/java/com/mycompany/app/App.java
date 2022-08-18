63d44290c1059be8aabcfb952b3e175e91ecf996
Chris Wininger <cwininger@sonatype.com>
2022-08-18 13:56:42 -0400

    first commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,25 @@
+package com.mycompany.app;
+
+public class App {
+  public static void main(String [] args) {
+    System.out.println("Hello world");
+  }
+
+/*  private String getName() {
+    return "Unused name";
+  }
+
+  private String getDesc() {
+    return "Unused desc";
+  }
+
+  private String getCompany() {
+    return "Unused company";
+  }
+
+  private String getYear() {
+    return "Unused year";
+  }
+*/
+}
+
