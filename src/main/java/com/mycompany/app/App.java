ab90342425592f2132c49cab1670e36e7dea7d91
Chris Wininger <cwininger@sonatype.com>
2022-08-18 14:30:19 -0400

    remove functions

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,21 +4,5 @@ public class App {
   public static void main(String [] args) {
     System.out.println("Hello world");
   }
-
-  private String getName() {
-    return "Unused name";
-  }
-
-  private String getDesc() {
-    return "Unused desc";
-  }
-
-  private String getCompany() {
-    return "Unused company";
-  }
-
-  private String getYear() {
-    return "Unused year";
-  }
 }
 
