36d9fc2d69d2989728d3d3b01ccfdfa70c7f3388
Chris Wininger <cwininger@sonatype.com>
2022-08-19 12:36:29 -0400

    this could be null, but yolo
    what do you have to say squirrel?

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,6 +3,22 @@ package com.mycompany.app;
 public class App {
   public static void main(String [] args) {
     System.out.println("Hello world");
+
+    FooBar fooBar = new FooBar("much fun");
+
+    System.out.println("length bar: " + fooBar.getBar().length());
+  }
+}
+
+class FooBar {
+  private String value;
+
+  public FooBar(String value) {
+    this.value = value;
+  }
+
+  public String getBar() {
+    return this.value;
   }
 }
 
