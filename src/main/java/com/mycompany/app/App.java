48ca29e8156d8677ef4a4575764788c5a0f0fe99
Chris Wininger <cwininger@sonatype.com>
2022-08-25 12:49:53 -0400

    more lift testing

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,5 +4,21 @@ public class App {
   public static void main(String [] args) {
     System.out.println("Hello world");
   }
+
+  private String getName() {
+    return "Unused name";
+  }
+
+  private String getDesc() {
+    return "Unused desc";
+  }
+
+  private String getCompany() {
+    return "Unused company";
+  }
+
+  private String getYear() {
+    return "Unused year";
+  }
 }
 
