5a00f0bc848ee1dabf9f8e6b812d56d9db07b565
Chris Wininger <cwininger@sonatype.com>
2022-10-05 10:48:16 -0400

    much fun

---
+++ .lift/customLangTool.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+import json
+import sys
+import shutil
+import subprocess
+import os
+import re
+
+name = "CustomLang"
+apiVersion = 3
+
+def emitStartInfo():
+    info = { "version": apiVersion, "name": name }
+    print(json.dumps(info))
+
+def emitVersion():
+    print(json.dumps(apiVersion))
+
+def emitName():
+    print(name)
+
+def emitApplicable():
+    print(json.dumps(True))
+
+def run(runInfo):
+    print(json.dump({}))
+
+def finalize(args, info):
+    codedir = args[1]
+    langauage = info['language']
+
+    outputs = {
+        'toolNotes': [{
+            'type': 'using-crufty-language',
+            'message': '"{}" is crufty'.format(langauage),
+            'file': os.path.join(codedir, 'src/main/java/com/mycompany/app/App.java'),
+            'line': 1,
+            'column': 0
+        }]
+    }
+
+    print(json.dumps(outputs))
+
+def main():
+    args = sys.argv
+
+    info = json.loads(sys.stdin.read())
+
+    if (len(args) < 4):
+        emitStartInfo()
+    else:
+        cmd = args[3]
+        if cmd == "version":
+            emitVersion()
+        elif cmd == "name":
+            emitName()
+        elif cmd == "applicable":
+            emitApplicable()
+        elif cmd == "run":
+            run(info)
+        elif cmd == "finalize":
+            finalize(args, info)
+
+if __name__ == "__main__":
+    main()
