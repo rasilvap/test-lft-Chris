9bebe0390837338bb9e22ad3a3a24d3474a35f9b
Chris Wininger <cwininger@sonatype.com>
2022-10-20 13:47:07 -0400

    try try agin

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,6 +3,14 @@ package com.mycompany.app;
 public class App {
   public static void main(String [] args) {
     System.out.println("Hello world");
+
+    Derived d = new Derived();
+    Base b = (Base) d;
+
+    // redundant cast, should trigger xlint
+    Base c = (Base) new Base();
+    System.out.println(b);
+    System.out.println(c);
   }
 }
 
--- src/main/java/com/mycompany/app/Lift4698.java
+++ src/main/java/com/mycompany/app/Lift4698.java
@@ -1,9 +1,4 @@
 package com.mycompany.app;
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.HashMap;
-import java.util.Map;
-
 // test for an issue where xlint errors are treated as error prone findings
 public class Lift4698
 {
@@ -14,17 +9,9 @@ public class Lift4698
         Base b = (Base) d;
 
         // redundant cast, should trigger xlint
-        Base c = (Base) (new Base());
+        Base c = (Base) new Base();
         System.out.println(b);
         System.out.println(c);
     }
 }
 
-
-
-
-
-
-
-class Base {}
-class Derived extends Base {}
