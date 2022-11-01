99a03289d007d45da1996a0dfe62634ad402ca47
Chris Wininger <cwininger@sonatype.com>
2022-11-01 18:22:54 -0400

    much fun

---
+++ src/main/java/com/mycompany/app/EmptyTryCatch.java
@@ -0,0 +1,10 @@
+package com.mycompany.app;
+
+public class EmptyTryCatch
+{
+    public void doStuff() {
+        try {
+            System.out.println("doing a thing");
+        } catch (Exception ex) {}
+    }
+}
