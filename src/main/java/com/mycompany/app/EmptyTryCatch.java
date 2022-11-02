abb4ef500953f082fe5dafda0565cf400208fce7
Chris Wininger <cwininger@sonatype.com>
2022-11-02 10:56:14 -0400

    try again to get alwyas throws

--- src/main/java/com/mycompany/app/EmptyTryCatch.java
+++ src/main/java/com/mycompany/app/EmptyTryCatch.java
@@ -2,10 +2,13 @@ package com.mycompany.app;
 
 public class EmptyTryCatch
 {
-    public void doStuff() {
+    public void doStuff() {;
         try {
             System.out.println("doing a thing");
             throw new RuntimeException("I always do this");
         } catch (Exception ex) {}
+
+
+        throw new RuntimeException("WTF");
     }
 }
