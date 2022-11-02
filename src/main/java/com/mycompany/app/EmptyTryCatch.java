17da6a8bcfa3197ff3c229c52fc6a4720aa5e044
Chris Wininger <cwininger@sonatype.com>
2022-11-02 10:31:15 -0400

    always throws

--- src/main/java/com/mycompany/app/EmptyTryCatch.java
+++ src/main/java/com/mycompany/app/EmptyTryCatch.java
@@ -5,6 +5,7 @@ public class EmptyTryCatch
     public void doStuff() {
         try {
             System.out.println("doing a thing");
+            throw new RuntimeException("I always do this");
         } catch (Exception ex) {}
     }
 }
