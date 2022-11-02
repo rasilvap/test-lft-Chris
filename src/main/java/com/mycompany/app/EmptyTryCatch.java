703ee86fa1bd7d72c26578e75e6659064cecd79b
Chris Wininger <cwininger@sonatype.com>
2022-11-02 13:42:28 -0400

    much grr

--- src/main/java/com/mycompany/app/EmptyTryCatch.java
+++ src/main/java/com/mycompany/app/EmptyTryCatch.java
@@ -1,14 +1,22 @@
 package com.mycompany.app;
 
+import java.awt.*;
+
 public class EmptyTryCatch
 {
     public void doStuff() {;
         try {
             System.out.println("doing a thing");
+
+            // always throws (But not triggering for some reason)
             throw new RuntimeException("I always do this");
         } catch (Exception ex) {}
 
 
-        throw new RuntimeException("WTF");
+        // long to float conversion implicit https://errorprone.info/bugpattern/LongFloatConversion
+        Color temp =  new Color(Math.round(18.0), Math.round(0.0), Math.round(18.0));
+
+        // always throws (but not triggering for some reason)
+        throw new RuntimeException("WTF: " + temp);
     }
 }
