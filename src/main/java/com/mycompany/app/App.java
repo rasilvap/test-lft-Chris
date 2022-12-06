7c5391170b94bad4497420498f931c14cbca575b
Håkan Thörngren <hth313@gmail.com>
2022-12-06 12:34:38 -0800

    Alter a comment

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,7 +7,7 @@ public class App {
     Derived d = new Derived();
     Base b = (Base) d;
 
-    // new comment
+    // new comment changed again
     Base c = (Base) new Base();
     System.out.println(b);
     System.out.println(c);
